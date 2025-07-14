aaa014aabeaa9bb75875649d8bd5eb579784a255
Mrinali Charhate <163443152+mrinaliii@users.noreply.github.com>
2025-07-14 21:43:45 +0530

    Time: 31 ms (5.16%) | Memory: 12.6 MB (14.33%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
@@ -4,14 +4,12 @@ class Solution(object):
         print(d)
         for i in s:
             d[i] += 1
-        e = []
-        o =[]
-        for i in (d.values()):
-            if (i % 2 == 0):
-                e.append(i)
+        e = 99999
+        o = 0
+        for i in (d):
+            if (d[i] % 2 == 0):
+                e = min(e, d[i])
             else:
-                o.append(i)
-        x = max(o)
-        y = min(e)
-        return x-y
+                o = max(o, d[i])
+        return o-e
         
