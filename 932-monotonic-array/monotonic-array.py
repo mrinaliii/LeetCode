0a95c0d2c48f1d73123e6b9439cd2d8dc09bc541
Mrinali Charhate <163443152+mrinaliii@users.noreply.github.com>
2025-07-21 23:04:18 +0530

    Time: 69 ms (47.19%) | Memory: 20.4 MB (94.27%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -1,9 +1,22 @@
 class Solution(object):
     def isMonotonic(self, nums):
-        temp = sorted(nums)
-        temp2 = temp[::-1]
-        if(temp==nums or temp2==nums):
+        count1 = 0
+        count2 = 0
+        for i in range(1,len(nums)):
+            if(nums[i-1] < nums[i]):
+                count1 += 1
+            elif(nums[i-1] > nums[i]):
+                count2 += 1
+            elif(nums[i-1] == nums[i]):
+                continue
+
+        if(count1 == 0 or count2 == 0):
             return True
-        else:
-            return False
+        return False
+        # temp = sorted(nums)
+        # temp2 = temp[::-1]
+        # if(temp==nums or temp2==nums):
+        #     return True
+        # else:
+        #     return False
         
